fdc66dc37bda60992f380e663afe07665096a2a8
chakirardenne <ardenne.chakir@gmail.com>
2022-09-23 19:39:28 +0200

    refactor: refactor withdraw operation with negative balance handling

--- src/main/java/com/exalt/bankaccount/domain/impl/AccountImpl.java
+++ src/main/java/com/exalt/bankaccount/domain/impl/AccountImpl.java
@@ -1,5 +1,6 @@
 package com.exalt.bankaccount.domain.impl;
 
+import com.exalt.bankaccount.domain.exception.NegativeBalanceException;
 import com.exalt.bankaccount.domain.intf.Account;
 
 public class AccountImpl implements Account {
@@ -15,7 +16,9 @@ public class AccountImpl implements Account {
     }
 
     @Override
-    public void withdraw(double amount) {
+    public void withdraw(double amount) throws NegativeBalanceException {
+        if(this.balance < 0)
+            throw new NegativeBalanceException("Can't withdraw from account, balance is negative");
         this.balance-=amount;
     }
 
--- src/main/java/com/exalt/bankaccount/domain/intf/Account.java
+++ src/main/java/com/exalt/bankaccount/domain/intf/Account.java
@@ -1,7 +1,9 @@
 package com.exalt.bankaccount.domain.intf;
 
+import com.exalt.bankaccount.domain.exception.NegativeBalanceException;
+
 public interface Account {
     void deposit(double amount);
-    void withdraw(double amount);
+    void withdraw(double amount) throws NegativeBalanceException;
     double getBalance();
 }
