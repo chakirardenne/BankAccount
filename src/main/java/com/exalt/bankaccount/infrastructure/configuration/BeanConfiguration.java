a9343608261263458f94fb4d474e1c6f80e81cd1
chakirardenne <ardenne.chakir@gmail.com>
2022-09-24 23:28:42 +0200

    config: adding persistence with jpa repository and entities config files

--- src/main/java/com/exalt/bankaccount/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/exalt/bankaccount/infrastructure/configuration/BeanConfiguration.java
@@ -1,7 +1,9 @@
-package com.exalt.bankaccount.infrastructure;
+package com.exalt.bankaccount.infrastructure.configuration;
 
 import com.exalt.bankaccount.domain.ports.AccountRepository;
 import com.exalt.bankaccount.domain.service.BankAccountService;
+import com.exalt.bankaccount.infrastructure.adapters.MySQLDbRepository;
+import com.exalt.bankaccount.infrastructure.adapters.SpringDataJpaRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,9 @@ public class BeanConfiguration {
     @Bean
     BankAccountService accountService(AccountRepository accountRepository) {
         return new BankAccountService(accountRepository);
+    }
+    @Bean
+    AccountRepository accountRepository(SpringDataJpaRepository springDataJpaRepository) {
+        return new MySQLDbRepository(springDataJpaRepository);
     }
 }
