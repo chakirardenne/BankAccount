636cfb30bf7e3edd9d3b30906cd65eba70a919fd
chakirardenne <ardenne.chakir@gmail.com>
2022-09-26 10:49:36 +0200

    test: added test for rest adapter

---
+++ src/test/java/com/exalt/bankaccount/application/adapters/AccountRestControlerTest.java
@@ -0,0 +1,54 @@
+package com.exalt.bankaccount.application.adapters;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountRestControlerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void deposit() throws Exception {
+        mvc.perform(get("/accounts/deposit")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void withdraw() throws Exception {
+        mvc.perform(get("/accounts/withdraw")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getHistory() throws Exception {
+        mvc.perform(get("/accounts/{id}/history")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
