f45b01f929fef955b41eeccd44042a28964ed194
chakirardenne <ardenne.chakir@gmail.com>
2022-10-24 20:21:10 +0200

    refactor: refactoring transaction type, removing useless code

--- src/main/java/com/exalt/bankaccount/domain/impl/TransactionType.java
+++ src/main/java/com/exalt/bankaccount/domain/impl/TransactionType.java
@@ -1,12 +1,5 @@
 package com.exalt.bankaccount.domain.impl;
 
 public enum TransactionType {
-    DEPOSIT("DEPOSIT"),WITHDRAW("WITHDRAW");
-    private final String type;
-    TransactionType(String type) {
-        this.type = type;
-    }
-    public String getType() {
-        return type;
-    }
+    DEPOSIT, WITHDRAW
 }
