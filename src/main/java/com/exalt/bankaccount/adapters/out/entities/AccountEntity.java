7cfdb8bf0e23bc1458ceed7d1b73e9186a620c2b
chakirardenne <ardenne.chakir@gmail.com>
2022-10-25 22:11:03 +0200

    refactor(infrastructure): updating mapping method, changing immutable to mutable list creation

--- src/main/java/com/exalt/bankaccount/adapters/out/entities/AccountEntity.java
+++ src/main/java/com/exalt/bankaccount/adapters/out/entities/AccountEntity.java
@@ -3,18 +3,17 @@ package com.exalt.bankaccount.adapters.out.entities;
 import com.exalt.bankaccount.domain.impl.AccountImpl;
 import com.exalt.bankaccount.domain.intf.Account;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class AccountEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,17 +25,9 @@ public class AccountEntity {
     private List<TransactionEntity> transactions;
 
     public static Account toDomain(AccountEntity account) {
-        return new AccountImpl(account.getId(), account.getBalance(), account.getName());
-    }
-
-    public static AccountEntity toEntity(Account account) {
-        return AccountEntity.builder()
-            .id(account.getId())
-            .balance(account.getBalance())
-            .name(account.getName())
-            .transactions(account.getTransactionHistory().stream()
-                .map(TransactionEntity::toEntity)
-                .toList())
-            .build();
+        return new AccountImpl(account.getId(),
+                account.getBalance(),
+                account.getName(),
+                account.getTransactions().stream().map(TransactionEntity::toDomain).collect(Collectors.toList()));
     }
 }
