dad438c660c86d3b0ca16b964bdf112953bb6e28
chakirardenne <ardenne.chakir@gmail.com>
2022-10-26 16:26:58 +0200

    feat(application): adding exception handler for controller

--- src/main/java/com/exalt/bankaccount/configuration/ControllerExceptionHandler.java
+++ src/main/java/com/exalt/bankaccount/configuration/ControllerExceptionHandler.java
@@ -1,2 +1,41 @@
-package com.exalt.bankaccount.configuration;public class ControllerExceptionHandler {
+package com.exalt.bankaccount.configuration;
+
+import com.exalt.bankaccount.application.exception.AccountNotFoundException;
+import com.exalt.bankaccount.domain.exception.NegativeAmountInOperationException;
+import com.exalt.bankaccount.domain.exception.NegativeBalanceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(value = {AccountNotFoundException.class})
+    public ResponseEntity<Object> resourceNotFoundException(AccountNotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<Object>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {NegativeBalanceException.class})
+    public ResponseEntity<Object> negativeBalanceException(AccountNotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {NegativeAmountInOperationException.class})
+    public ResponseEntity<Object> negativeAmountInOperationException(AccountNotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+        return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
+    }
 }
