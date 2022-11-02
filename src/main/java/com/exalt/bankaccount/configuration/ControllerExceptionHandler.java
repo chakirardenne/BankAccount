7d64dfcb0fb611244c480d10ec5671f05b8315e2
chakirardenne <ardenne.chakir@gmail.com>
2022-11-02 15:13:22 +0100

    refactor(application): adding exception handler case for HttpMessageNotReadableException in controller advice config

--- src/main/java/com/exalt/bankaccount/configuration/ControllerExceptionHandler.java
+++ src/main/java/com/exalt/bankaccount/configuration/ControllerExceptionHandler.java
@@ -5,6 +5,9 @@ import com.exalt.bankaccount.domain.exception.NegativeAmountInOperationException
 import com.exalt.bankaccount.domain.exception.NegativeBalanceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -36,5 +39,24 @@ public class ControllerExceptionHandler {
         body.put("timestamp", LocalDateTime.now());
         body.put("message", ex.getMessage());
         return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = {HttpMessageNotReadableException.class})
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Malformed JSON request");
+        return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
     }
 }
