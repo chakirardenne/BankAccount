d79816b72c41804d2a46df0138bb975602fae60b
chakirardenne <ardenne.chakir@gmail.com>
2022-11-03 15:45:08 +0100

    refactor(application): removing not used builder, now using mapper

--- src/main/java/com/exalt/bankaccount/adapters/in/dto/TransactionResponse.java
+++ src/main/java/com/exalt/bankaccount/adapters/in/dto/TransactionResponse.java
@@ -1,23 +1,11 @@
 package com.exalt.bankaccount.adapters.in.dto;
 
-import com.exalt.bankaccount.domain.intf.Transaction;
-import lombok.Builder;
 import lombok.Data;
 import java.time.LocalDateTime;
 @Data
-@Builder
 public class TransactionResponse {
     private String type;
     private double amount;
     private LocalDateTime date;
     private double balance;
-
-    public static TransactionResponse of(Transaction transaction) {
-        return TransactionResponse.builder()
-                .type(transaction.getTransactionType().name())
-                .amount(transaction.getAmount())
-                .date(transaction.getDate())
-                .balance(transaction.getBalance())
-                .build();
-    }
 }
