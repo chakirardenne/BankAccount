a2ffa382808f942b5485f19ed8458fa4c3e02563
chakirardenne <ardenne.chakir@gmail.com>
2022-11-03 08:48:05 +0100

    refactor(application): refactor exception handler

--- src/main/java/com/exalt/bankaccount/configuration/ControllerExceptionHandler.java
+++ src/main/java/com/exalt/bankaccount/configuration/ControllerExceptionHandler.java
@@ -4,59 +4,50 @@ import com.exalt.bankaccount.application.exception.AccountNotFoundException;
 import com.exalt.bankaccount.domain.exception.NegativeAmountInOperationException;
 import com.exalt.bankaccount.domain.exception.NegativeBalanceException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler {
-    @ExceptionHandler(value = {AccountNotFoundException.class})
-    public ResponseEntity<Object> resourceNotFoundException(AccountNotFoundException ex) {
+    @ExceptionHandler(value = {
+            AccountNotFoundException.class,
+            NegativeBalanceException.class,
+            NegativeAmountInOperationException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object badRequestBusinessException(RuntimeException ex) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("message", ex.getMessage());
-        return new ResponseEntity<Object>(body, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(value = {NegativeBalanceException.class})
-    public ResponseEntity<Object> negativeBalanceException(NegativeBalanceException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", ex.getMessage());
-        return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(value = {NegativeAmountInOperationException.class})
-    public ResponseEntity<Object> negativeAmountInOperationException(NegativeAmountInOperationException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", ex.getMessage());
-        return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
+        return body;
     }
 
     @ExceptionHandler(value = {HttpMessageNotReadableException.class})
-    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("message", "Malformed JSON request");
-        return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
+        return body;
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Object handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
+        return errors;
     }
 }
