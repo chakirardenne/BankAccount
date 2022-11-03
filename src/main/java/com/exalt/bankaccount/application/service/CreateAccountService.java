d3a179db6bb3bc9b3745c08b9f3632d0a6fab070
chakirardenne <ardenne.chakir@gmail.com>
2022-11-03 14:19:38 +0100

    refactor(application): refactoring create use case adapter, adding balance value checking

--- src/main/java/com/exalt/bankaccount/application/service/CreateAccountService.java
+++ src/main/java/com/exalt/bankaccount/application/service/CreateAccountService.java
@@ -4,12 +4,18 @@ import com.exalt.bankaccount.adapters.in.dto.CreateAccountRequest;
 import com.exalt.bankaccount.adapters.in.dto.CreateAccountResponse;
 import com.exalt.bankaccount.application.ports.in.CreateAccountUseCase;
 import com.exalt.bankaccount.application.ports.out.AccountPort;
+import com.exalt.bankaccount.domain.exception.IncorrectAmountValueInOperationException;
 import com.exalt.bankaccount.domain.impl.AccountImpl;
+import com.exalt.bankaccount.domain.impl.TransactionImpl;
+import com.exalt.bankaccount.domain.impl.TransactionType;
 import com.exalt.bankaccount.domain.intf.Account;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +26,12 @@ public class CreateAccountService implements CreateAccountUseCase {
     @Transactional
     @Override
     public CreateAccountResponse create(CreateAccountRequest createAccountRequest) {
-        Account account = accountPort.save(new AccountImpl(
-                createAccountRequest.getBalance(),
-                createAccountRequest.getName()));
+        if(createAccountRequest.getBalance() <= 0)
+            throw new IncorrectAmountValueInOperationException("Starting amount must be strictly positive");
+        Account account = new AccountImpl(createAccountRequest.getBalance(),createAccountRequest.getName());
+        account.addTransaction(new TransactionImpl(TransactionType.DEPOSIT, account.getBalance(),
+                LocalDateTime.now(ZoneOffset.UTC), account.getBalance()));
+        account = accountPort.save(account);
         return modelMapper.map(account, CreateAccountResponse.class);
     }
 }
