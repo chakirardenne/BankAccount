df20529100da7a00a5b6599d31a4d8a9bb06faa7
chakirardenne <ardenne.chakir@gmail.com>
2022-11-03 08:12:58 +0100

    refactor(domain): removing lombok annotation

--- src/main/java/com/exalt/bankaccount/domain/impl/TransactionImpl.java
+++ src/main/java/com/exalt/bankaccount/domain/impl/TransactionImpl.java
@@ -1,16 +1,21 @@
 package com.exalt.bankaccount.domain.impl;
 
 import com.exalt.bankaccount.domain.intf.Transaction;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
 public class TransactionImpl implements Transaction {
     private final TransactionType type;
     private final double amount;
     private final LocalDateTime date;
     private final double balance;
+
+    public TransactionImpl(TransactionType type, double amount, LocalDateTime date, double balance) {
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+        this.balance = balance;
+    }
 
     @Override
     public TransactionType getTransactionType() {
